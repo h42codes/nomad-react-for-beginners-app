733472ea5f286e06fc7c7cb6b8acf7eb5b1d79fb
h42codes <99951343+h42codes@users.noreply.github.com>
2022-11-26 00:33:56 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// useEffect's first arg -> the code that we want to run only once
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   // we want some code to run ONLY on the first component render
-  console.log("call an api");
+  console.log("I ran all the time.");
+  // const iRunOnlyOnce = () => {
+  //   console.log("I run only once.");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
