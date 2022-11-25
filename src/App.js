240aa5a8f3acaa59d77e37bcb7b98e07347087de
h42codes <99951343+h42codes@users.noreply.github.com>
2022-11-26 01:08:14 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  // console.log("I ran all the time.");
+// execute a code when a component is destroyed
+
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("Created!");
+    // This is a cleanup function that allos you to do something when the component is destroyed
+    return () => console.log("Destroyed"); // this is run when the component is destroyed
+  }, []); // having no dependencies means it runs only one time
+  return <h1>Hello</h1>;
+}
+
+function Hello2() {
+  // Cleanup function
+  function byFn() {
+    console.log("bye");
+  }
+  function hiFn() {
+    console.log("hi");
+    // when the component is destroyed, react will run the function returned by this function
+    return byFn;
+  }
+  useEffect(hiFn, []);
+
+  useEffect(() => {
+    console.log("HI");
+    return () => {
+      console.log("BYE");
+    };
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); // run only when keyword changes!
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes.");
-  }, [keyword, counter]);
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
