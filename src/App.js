054ad579c270df3cfb4e84d1a60f8361ede5b4c4
h42codes <99951343+h42codes@users.noreply.github.com>
2022-11-26 00:50:01 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import { useState, useEffect } from "react";
 
-// useEffect's first arg -> the code that we want to run only once
-
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  // we want some code to run ONLY on the first component render
-  console.log("I ran all the time.");
-  // const iRunOnlyOnce = () => {
-  //   console.log("I run only once.");
-  // };
-  // useEffect(iRunOnlyOnce, []);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  // console.log("I ran all the time.");
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]); // run only when keyword changes!
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click</button>
     </div>
