5e5896f2534b012f6eca3da1d5d5f4d6e80a5a4e
h42codes <99951343+h42codes@users.noreply.github.com>
2022-11-26 20:45:43 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,61 +1,24 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
+// HashRouter
+// You can render two routes at once, but we use Switch to do one at a time
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  // useEffect(() => {
-  //   fetch(
-  //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setMovies(json.data.movies);
-  //       setLoading(false);
-  //     });
-  // }, []);
-  const getMoviesV1 = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    );
-    const json = await response.json;
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello!</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
